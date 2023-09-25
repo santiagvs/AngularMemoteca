5f85d0079930cf6ad142457dc8ecbfcb12a460c6
Santiago <victor.lacsilva0102@gmail.com>
2023-09-25 01:15:58 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { PensamentosComponent } from './components/pensamentos/pensamentos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    PensamentosComponent
   ],
   imports: [
     BrowserModule,
